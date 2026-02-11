5e3498ae0ba20356a7fe3330fa6a716b66e20716
= <lukejmullins@gmail.com>
2026-02-11 10:10:37 -0500

    Fixed form name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Form from "./components/form";
+import Form from "./components/Form";
 import Display from "./components/Display";
 import "./styles/app.css";
 
